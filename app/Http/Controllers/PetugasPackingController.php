6350fc85b0f0492fadd412788bb906acbc810519
Mirza Priscilla Faradiba <mirzapriscilla5@gmail.com>
2023-04-11 09:48:24 +0700

    Migration - Tabel PetugasPacking

---
+++ app/Http/Controllers/PetugasPackingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PetugasPacking;
+use Illuminate\Http\Request;
+
+class PetugasPackingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PetugasPacking  $petugasPacking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PetugasPacking $petugasPacking)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PetugasPacking  $petugasPacking
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PetugasPacking $petugasPacking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PetugasPacking  $petugasPacking
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PetugasPacking $petugasPacking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PetugasPacking  $petugasPacking
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PetugasPacking $petugasPacking)
+    {
+        //
+    }
+}
