113333a58cf96ea7224485e20cd16ae59150d656
KurniawatiAgustina <kurniawatiagustina450@gmail.com>
2023-04-11 09:43:11 +0700

    Migration - Tabel Petugas cuci

---
+++ app/Http/Controllers/PetugasCuciController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Petugas_cuci;
+use Illuminate\Http\Request;
+
+class PetugasCuciController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Petugas_cuci  $petugas_cuci
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Petugas_cuci $petugas_cuci)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Petugas_cuci  $petugas_cuci
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Petugas_cuci $petugas_cuci)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Petugas_cuci  $petugas_cuci
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Petugas_cuci $petugas_cuci)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Petugas_cuci  $petugas_cuci
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Petugas_cuci $petugas_cuci)
+    {
+        //
+    }
+}
