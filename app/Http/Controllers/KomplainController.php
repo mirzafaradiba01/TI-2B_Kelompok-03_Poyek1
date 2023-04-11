48575f3686686dacdff66e62d6916f48b72dcd4d
KurniawatiAgustina <kurniawatiagustina450@gmail.com>
2023-04-11 09:54:31 +0700

    Migration - Komplain

---
+++ app/Http/Controllers/KomplainController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Komplain;
+use Illuminate\Http\Request;
+
+class KomplainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Komplain  $komplain
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Komplain $komplain)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Komplain  $komplain
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Komplain $komplain)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Komplain  $komplain
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Komplain $komplain)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Komplain  $komplain
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Komplain $komplain)
+    {
+        //
+    }
+}
