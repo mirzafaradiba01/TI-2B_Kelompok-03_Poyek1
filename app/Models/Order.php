b2b20e41bf14f6ef9327933c4babfa2e132af89f
Mirza Priscilla Faradiba <mirzapriscilla5@gmail.com>
2023-05-02 10:38:20 +0700

    Migration - Tabel Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $table = 'order';
+    protected $fillable = [
+        'kode_order',
+        'nota_order',
+        'berat_laundry',
+        'total_laundry',
+        'catatan_laundry',
+        'status_laundry',
+        'status_bayar'
+    ];
 }
--- database/migrations/2023_04_11_023101_create_orders_table.php
+++ database/migrations/2023_04_11_023101_create_orders_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('order', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('kode_order')->unsigned()->unique();
+            $table->unsignedInteger('kode_JL')->unsigned();
+            $table->unsignedInteger('kode_pelanggan')->unsigned();
+            $table->string('nota_order')->nullable();
+            $table->integer('berat_laundry' )->nullable();
+            $table->integer('total_laundry')->nullable();
+            $table->string('catatan_laundry')->nullable();
+            $table->string('status_laundry')->nullable();
+            $table->string('status_bayar')->nullable();
             $table->timestamps();
         });
     }
