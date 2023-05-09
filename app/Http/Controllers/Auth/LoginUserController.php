3483dfdcf492c52dde7ee4a49d8a773a517b7282
Shofiatul Ayu Anikaningrum <shofiaa060@gmail.com>
2023-05-09 08:50:43 +0700

    Login User

---
+++ app/Http/Controllers/Auth/LoginUserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use Illuminate\Http\Request;
+
+
+class LoginUserController extends Controller
+{
+    //
+}
