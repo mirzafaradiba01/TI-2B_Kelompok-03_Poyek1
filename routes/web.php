4e7877680d68717521e81e85f0af34454a392634
Shofiatul Ayu Anikaningrum <shofiaa060@gmail.com>
2023-05-09 09:48:00 +0700

    Tampilan Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('/dashboard', DashboardController::class);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
