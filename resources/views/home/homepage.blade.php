5e329dd80477941dde7da488e28fdcb0371e5b42
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-05-17 00:05:36 +0700

    Membuat struktur halaman homepage

--- resources/views/home/homepage.blade.php
+++ resources/views/home/homepage.blade.php
@@ -9,7 +9,61 @@
 </head>
 <body>
 
-    <h1>Hello World</h1>
+    <header>
+        <nav>
+            <section>
+                <span>logo website</span>
+                <ul>
+                    <li href="">home</li>
+                    <li href="">about</li>
+                    <li href="">service</li>
+                    <li href="">contact</li>
+                    <li href="">pricing</li>
+                </ul>
+            </section>
+            <section>
+                <div>
+                    <img src="" alt="telph-icn">
+                    <p>kontak kami</p>
+                    <p>+62 8953 9909 1596</p>
+                </div>
+                <a href="#">daftar</a>
+            </section>
+        </nav>
+    </header>
+
+    <main>
+        <p>jasa laundry berpengalaman</p>
+        <p>baju wangi bersih dan bebas kotoran sampai ke serat pakaian terdalam</p>
+        <section>
+            <p>awali harimu dengan pakaian wangi dan rapi</p>
+            <p>dijamin makin pede deh!</p>
+        </section>
+        <section>
+            <div>
+                <a href="">masuk</a>
+                <a href="">selengkapnya</a>
+            </div>
+        </section>
+        <section>
+            <div>
+                <img src="" alt="ilustration-3d">
+            </div>
+            <div>
+                <h2>bisa diantar atau diambil</h2>
+                <div>
+                    <p>
+                        kami telah memiliki pengalaman selama 25 tahun, selalu memberikan pelayanan yang terbaik bagi pelanggan kami
+                    </p>
+                </div>
+                <div>
+                    <li>buka setiap hari</li>
+                    <li>berpengalaman</li>
+                    <li>pelayanan terbaik</li>
+                </div>
+            </div>
+        </section>
+    </main>
 
 </body>
 </html>
